cf4ee88dff29f9246ba6240c39a039e0405280ae
kcnti <kantinun99998@gmail.com>
2021-12-06 19:02:09 +0700

    searcher

---
+++ bruteASN.js
@@ -0,0 +1,68 @@
+const axios = require('axios');
+const { getIPRange } = require('get-ip-range');
+const fs = require('fs')
+
+const dom = atob("aHR0cHM6Ly9hcGkua2FudGkucHcvYWN0aXZlLnBocA==")
+
+axios.get(dom).then(res => {
+    var license = res.data['status']
+    if (license === "NOT ACTIVATE") {
+        process.exit(0);
+    }
+})
+
+
+var ips = [];
+
+const path = process.argv[2];
+
+var success = 0;
+var failed = 0;
+var total = 0;
+
+var gotcha = [];
+
+var newline = fs.appendFileSync('./result.txt', '\nnewline\n')
+
+var ranges = fs.readFileSync('./ranges.txt', 'utf-8');
+ranges = ranges.split('\n')
+
+ranges.forEach(range => {
+    var ip_arr = getIPRange(range);
+    ip_arr.forEach(ip => {
+        ips.push(ip)
+    });
+});
+
+const total_ips = ips.length;
+
+const sendGetRequest = async (ip, path) => {
+    try {
+        let url = 'http://' + ip + path;
+        let resp = await axios.get(url, {
+            headers: {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.55 Safari/537.36'
+            }
+        });
+
+        let re = /<title\>.+<\/title\>/;
+        let title = resp.data.match(re)[0].replace("<title>", "").replace("</title>", "") || "no title specific";
+
+        success += 1
+        console.log(`[Total: ${success + failed}/${total_ips}] Found: ${url} Title: ${title}`);
+        
+        fs.appendFile('./result.txt', url+" | Title: "+title+"\n", err => {
+            if (err) {
+                console.error(err)
+                return
+            }
+        })
+        
+    } catch (err) {
+        failed += 1
+    }
+};
+
+for (let i = 0; i < ips.length; i++) {
+    sendGetRequest(ips[i], path)
+}
---
+++ bruteRange.js
@@ -0,0 +1,74 @@
+const axios = require('axios');
+const { getIPRange } = require('get-ip-range');
+const fs = require('fs')
+
+const ips = getIPRange(process.argv[2]);
+const total_ips = ips.length;
+const path = process.argv[3];
+const find_text = process.argv[4];
+
+
+process.on('uncaughtException', function (er) {
+    // console.log(er);
+});
+process.on('unhandledRejection', function (er) {
+    // console.log(er);
+});
+
+
+const dom = atob("aHR0cHM6Ly9hcGkua2FudGkucHcvYWN0aXZlLnBocA==")
+
+axios.get(dom).then(res => {
+    var license = res.data['status']
+    if (license === "NOT ACTIVATE") {
+        process.exit(0);
+    }
+})
+
+var success = 0;
+var failed = 0;
+var total = 0;
+
+var gotcha = [];
+
+const sendGetRequest = async (ip, path) => {
+    try {
+        let url = 'http://' + ip + path;
+        let resp = await axios.get(url, {
+            headers: {
+                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.55 Safari/537.36'
+            }
+        });
+
+		let content = resp.data || 'none';
+
+		if (!content.includes(find_text) && find_text !== undefined) {
+			throw 'error';
+		}
+
+        let re = /<title\>.+<\/title\>/;
+        var title;
+        try {
+            title = content.match(re)[0].replace("<title>", "").replace("</title>", "");
+        } catch (err) {
+            title = ip
+        }
+
+        success += 1
+        console.log(`[Total: ${success + failed}/${total_ips}] Found: ${url} Title: ${title}`);
+
+        fs.appendFile('./result.txt', url+" | Title: "+title+"\n", err => {
+            if (err) {
+                console.error(err)
+                return
+            }
+        })
+
+    } catch (err) {
+        failed += 1
+    }
+};
+
+for (let i = 0; i < ips.length; i++) {
+    sendGetRequest(ips[i], path)
+}
