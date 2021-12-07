0d9359927c9e71dc9adf5211d7d1d5e4ab740864
kcnti <kantinun99998@gmail.com>
2021-12-07 09:43:04 +0700

    searcher

--- bruteRange.js
+++ bruteRange.js
@@ -1,26 +1,25 @@
 const axios = require('axios');
-const { getIPRange } = require('get-ip-range');
-const dns = require('dns').promises;
-const dig = require('node-dig-dns');
+const cidrRange = require('cidr-range');
+const dns = require('dns');
 const fs = require('fs')
 
-const ips = getIPRange(process.argv[2]);
+const ips = cidrRange(process.argv[2]);
 const total_ips = ips.length;
 const path = process.argv[3];
+
+console.log(`total: ${total_ips}`)
 
 var success = 0;
 var failed = 0;
 
-const sendGetRequest = async (ip, path) => {
+const sendGetRequest = (ip, path) => {
     var url = 'http://' + ip + path;
-    try {
-        let resp = await axios.get(url, {
-            headers: {
-                'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.55 Safari/537.36'
-            }
-        });
-
-		let content = resp.data || 'none';
+    axios.get(url, {
+        headers: {
+            'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/96.0.4664.55 Safari/537.36'
+        }
+    }).then((response) => {
+        let content = response.data || 'none';
 
         let re = /<title\>.+<\/title\>/;
         var title;
@@ -29,32 +28,47 @@ const sendGetRequest = async (ip, path) => {
         } catch (err) {
             title = ip
         }
-        
-        var domain;
-        try {
-            domain = await dns.reverse(ip);
-            domain = domain.join(',')
-        } catch (e) {
-            domain = "NotFound"
-        }
+
+        // var domain;
+        // try {
+        //     domain = dns.reverse(ip);
+        //     domain = domain.join(',')
+        // } catch (e) {
+        //     domain = "NotFound"
+        // }
 
         success += 1
-        console.log(`[Total: ${success + failed}/${total_ips}] Found: ${url} DNS: ${domain} Title: ${title}`);
-        
-        fs.appendFile('./result.txt', url+" | DNS: "+domain+" | Title: "+title+"\n", err => {
+        console.log(`[Total: ${success}:${failed}-${success + failed}/${total_ips}] Found: ${url} Title: ${title}`);
+
+        fs.appendFile('./result.txt', url + " | Title: " + title + "\n", err => {
             if (err) {
                 console.error(err)
                 return
             }
         })
-        
-    } catch (err) {
+    }).catch((error) => {
         failed += 1
-        // console.log(err)
-        // console.log(`[Total: ${success + failed}/${total_ips}] Failed ${url}`);
-    }
+        let content = error.response.data || 'none';
+
+        let re = /<title\>.+<\/title\>/;
+        var title;
+        try {
+            title = content.match(re)[0].replace("<title>", "").replace("</title>", "");
+        } catch (err) {
+            title = ip
+        }
+        console.log(`[Total: ${success}:${failed}-${success + failed}/${total_ips}] Found: ${url} Title: ${title} Error ${error.response.status}`);
+
+        fs.appendFile('./result.txt', "Error " + url + " | Title: " + title + "\n", err => {
+            if (err) {
+                console.error(err)
+                return
+            }
+        })
+    });;
 };
 
 for (let i = 0; i < ips.length; i++) {
     sendGetRequest(ips[i], path)
+    // break
 }
---
+++ test/cidr.js
@@ -0,0 +1,3 @@
+var cidrRange = require('cidr-range');
+
+console.log(cidrRange('137.74.0.0/16'));
