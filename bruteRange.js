38d64b6b2832b3367a00b7f3a2bd2427aa2c27c5
kcnti <kantinun99998@gmail.com>
2023-10-21 16:31:44 +0700

    lili

--- bruteRange.js
+++ bruteRange.js
@@ -6,6 +6,7 @@ const fs = require('fs')
 const ips = cidrRange(process.argv[2]);
 const total_ips = ips.length;
 const path = process.argv[3];
+const output = process.argv[4] + '.txt' || 'result.txt';
 
 console.log(`total: ${total_ips}`)
 
@@ -40,7 +41,7 @@ const sendGetRequest = (ip, path) => {
         success += 1
         console.log(`[Total: ${success}:${failed}-${success + failed}/${total_ips}] Found: ${url} Title: ${title}`);
 
-        fs.appendFile('./result.txt', url + " | Title: " + title + "\n", err => {
+        fs.appendFile('./' + output, url + " | Title: " + title + "\n", err => {
             if (err) {
                 console.error(err)
                 return
@@ -48,7 +49,8 @@ const sendGetRequest = (ip, path) => {
         })
     }).catch((error) => {
         failed += 1
-        let content = error.response.data || 'none';
+        // let content = error.response.data || 'none';
+        let content = 'none';
 
         let re = /<title\>.+<\/title\>/;
         var title;
@@ -57,18 +59,43 @@ const sendGetRequest = (ip, path) => {
         } catch (err) {
             title = ip
         }
-        console.log(`[Total: ${success}:${failed}-${success + failed}/${total_ips}] Found: ${url} Title: ${title} Error ${error.response.status}`);
+        // console.log(`[Total: ${success}:${failed}-${success + failed}/${total_ips}] Found: ${url} Title: ${title} Error ${error.response.status}`);
 
-        fs.appendFile('./result.txt', "Error " + url + " | Title: " + title + "\n", err => {
-            if (err) {
-                console.error(err)
-                return
-            }
-        })
+        // fs.appendFile('./result.txt', "Error " + url + " | Title: " + title + "\n", err => {
+        //     if (err) {
+        //         console.error(err)
+        //         return
+        //     }
+        // })
     });;
 };
 
-for (let i = 0; i < ips.length; i++) {
-    sendGetRequest(ips[i], path)
-    // break
-}
+// for (let i = 0; i < ips.length; i++) {
+//     sendGetRequest(ips[i], path)
+//     // break
+// }
+
+const main = async () => {
+    console.log("IPs", total_ips);
+  
+    // Specify the batch size and delay between batches
+    const batchSize = 100; // Adjust the batch size as needed
+    const delayMs = 1000; // Adjust the delay as needed (1000ms = 1 second)
+  
+    for (let i = 0; i < ips.length; i += batchSize) {
+      const batch = ips.slice(i, i + batchSize);
+      // Create an array of promises for the current batch
+      const requestPromises = batch.map((ip) => sendGetRequest(ip, path));
+  
+      // Execute the requests in parallel for the current batch
+      await Promise.all(requestPromises);
+  
+      // Introduce a delay before the next batch if there are more IP addresses
+      if (i + batchSize < ips.length) {
+        await new Promise((resolve) => setTimeout(resolve, delayMs));
+      }
+    }
+  };
+  
+
+main()
